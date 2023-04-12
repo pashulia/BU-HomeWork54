4b30b746dc653e7f2a0e099bd17e60fce55db5cd
Pavel Khegay <pashulia.pk@gmail.com>
2023-04-12 21:07:48 +0600

    add task5

--- tasks/task4.js
+++ tasks/task4.js
@@ -5,34 +5,45 @@ async function task_4() {
     const privateKey = readlineSync.question('Введите закрытый ключь: ');
     const recipient = readlineSync.question('Введите адрес получателя: ');
     const value = readlineSync.question('Введите сумму: ');
+    
     let provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:7545');
-    let signer = new ethers.Wallet(privateKey, provider);
-    let signer2 = new ethers.Wallet('0xbd7bbb45c7d9c369194c725e6a10b8c6f7cdc9ed8e576ec70f1ebe84d5407b96', provider);
 
-    let transactionToSigner = {
+    let signer = new ethers.Wallet(privateKey);
+    signer = signer.connect(provider);
+
+    let signer2 = new ethers.Wallet('0xbd7bbb45c7d9c369194c725e6a10b8c6f7cdc9ed8e576ec70f1ebe84d5407b96');
+    signer2 = signer2.connect(provider);
+
+    await signer.getBalance().then(bal => {
+        console.log('signer balance 1: ',  bal.toBigInt());
+    })
+    let transactionToSigner = await signer2.populateTransaction({
         to: signer.address,
-        value: 1000000000000
-    }
-    await signer2.sendTransaction(transactionToSigner);
-    await signer.getBalance().then(console.log)
+        value: ethers.BigNumber.from(1000000000000000)
+    });
+    let tx = await signer2.sendTransaction(transactionToSigner);
+    let txReceipt = await tx.wait();
+    console.log('Transaction from account to signer:\n', txReceipt);
+    await signer.getBalance().then(bal => {
+        console.log('signer balance 2: ',  bal.toBigInt());
+    })
+
+    let transactionToRecepient = await signer.populateTransaction({
+        to: recipient,
+        value: ethers.BigNumber.from(value),
+    });
+    tx = await signer.sendTransaction(transactionToRecepient);
+    txReceipt = await tx.wait();
 
     const gasPrice = await provider.getGasPrice();
     const gasLimit = 21000;
     const txCost = gasPrice.mul(gasLimit);
-    const balanceBefore = await signer.getBalance();
-
-    let transactionRequest = {
-        to: recipient.address,
-        value: Number(value)
-    }
-
-    await signer.sendTransaction(transactionRequest);
-    const balanceAfter = await signer.getBalance();
-    const balanceDiff = balanceBefore.sub(balanceAfter).sub(txCost);
 
     console.log(`Отправлено ${ethers.utils.formatEther(value)} ETH с адреса ${signer.address} на адрес ${recipient}`);
     console.log(`Операция выполнена с суммарной комиссией в размере ${ethers.utils.formatEther(txCost)} ETH`);
-    console.log(`Новый баланс аккаунта: ${ethers.utils.formatEther(balanceDiff)} ETH`);
+    await signer.getBalance().then(bal => {
+        console.log('Новый баланс аккаунта: ',  bal.toBigInt());
+    })
 }
 
 task_4();
